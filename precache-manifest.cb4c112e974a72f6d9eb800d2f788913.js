03eeb24f87b710e8f5428ad4f8f5fa1a3774fd15
Danya_komlev <komlevdanila742@gmail.com>
2020-09-17 21:41:10 +0300

    Updates

---
+++ precache-manifest.cb4c112e974a72f6d9eb800d2f788913.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d6a57006d2176ddbeb2665c01e7d24",
+    "url": "/Pods-VKMA/index.html"
+  },
+  {
+    "revision": "89b6b4706e8ed5c74ff7",
+    "url": "/Pods-VKMA/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "817798d2eae6267cb132",
+    "url": "/Pods-VKMA/static/css/main.439b4a20.chunk.css"
+  },
+  {
+    "revision": "89b6b4706e8ed5c74ff7",
+    "url": "/Pods-VKMA/static/js/2.a6bb197d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Pods-VKMA/static/js/2.a6bb197d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817798d2eae6267cb132",
+    "url": "/Pods-VKMA/static/js/main.fc61e140.chunk.js"
+  },
+  {
+    "revision": "16425cc245df0d966cc2",
+    "url": "/Pods-VKMA/static/js/runtime-main.2d2affab.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Pods-VKMA/static/media/persik.4e1ec840.png"
+  }
+]);
